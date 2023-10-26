af3213f84af99bb3a3d4f4ee2ab1d137eba8429e
Kim-Minjae9872 <141595332+Kim-Minjae9872@users.noreply.github.com>
2023-10-27 00:39:51 +0900

    recommend image +

--- static/js/map_recommend.js
+++ static/js/map_recommend.js
@@ -13,7 +13,7 @@ async function printJSON(file_path) {
 }
 
 // 리스트 정렬하는 함수
-function read_list(list_json) {
+async function read_list(list_json) {
   const restaurant_list = document.getElementById(('card_list'))
   for (var r in list_json) {
     const restaurant = list_json[r]
@@ -21,9 +21,12 @@ function read_list(list_json) {
 
     // 리스트에 속성 추가
     const restaurant_article = document.createElement('article')
+    const restaurant_image = document.createElement('img')
+    restaurant_image.setAttribute("class", "card__background")
+    restaurant_image.setAttribute("src", restaurant.image)
     restaurant_article.setAttribute("class", "card")
-    const restaurant_card = document.createElement('div')
-    restaurant_card.setAttribute("class", "card__content | flow")
+    const restaurant_card = await document.createElement('div')
+    await restaurant_card.setAttribute("class", "card__content | flow")
     const restaurant_card_container = document.createElement('div')
     restaurant_card_container.setAttribute("class", "card__content--container | flow")
     const restaurant_name = document.createElement('h2')
@@ -45,7 +48,9 @@ function read_list(list_json) {
     else {
       restaurant_distance.innerText = `가게까지의 거리: ${restaurant.distance}km`
     }
-    restaurant_article.appendChild(restaurant_card)
+
+    restaurant_article.appendChild(restaurant_image)
+    await restaurant_article.appendChild(restaurant_card)
     restaurant_card.appendChild(restaurant_card_container)
     restaurant_card_container.appendChild(restaurant_name)
     restaurant_card_container.appendChild(restaurant_score)
