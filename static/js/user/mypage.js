c2e4b62e4c337e4af838430467c94a1ae99497c5
JungGyu00 <co399955@gmail.com>
2023-10-27 09:35:40 +0900

    comment: login.js, mypage.js, profile.js

--- static/js/user/mypage.js
+++ static/js/user/mypage.js
@@ -1,20 +1,19 @@
 window.onload = () => {
-    loadMypage()
+    loadMypage() // 마이페이지 로딩 함수 호출
 }
 
 function userIdSearch() {
     const url = window.location.href
     const url_split = url.split(["?"])[1]
-    const user_id = url_split.split(["="])[1]
+    const user_id = url_split.split(["="])[1] // URL에서 유저 아이디 추출
 
     return user_id
 }
 
-    
-    
+// 마이페이지 로딩 함수
 async function loadMypage() {
 
-    const user_id = userIdSearch()
+    const user_id = userIdSearch() // 유저 아이디 가져오기
 
     const response = await fetch(`http://127.0.0.1:8000/user/mypage/${user_id}/`, {
         method : 'GET'
@@ -23,38 +22,34 @@ async function loadMypage() {
     console.log(response_json)
     console.log(response_json.nickname)
 
+    const user_image = document.getElementById("profile_image") // 프로필 이미지 엘리먼트 가져오기
+    const nickname = document.getElementById("nickname") // 닉네임 엘리먼트 가져오기
+    const following_num = document.getElementById("following_count") // 팔로잉 수 엘리먼트 가져오기
+    const follower_num = document.getElementById("follower_count") // 팔로워 수 엘리먼트 가져오기
 
-    const user_image = document.getElementById("profile_image")
-    const nickname = document.getElementById("nickname")
-    const following_num = document.getElementById("following_count")
-    const follower_num = document.getElementById("follower_count")
-
-    nickname.innerText = response_json.nickname
-    following_num.innerText = response_json.following_count
-    follower_num.innerText = response_json.follower_count
+    nickname.innerText = response_json.nickname // 닉네임 표시
+    following_num.innerText = response_json.following_count // 팔로잉 수 표시
+    follower_num.innerText = response_json.follower_count // 팔로워 수 표시
     const user_image_img = document.createElement('img')
     user_image_img.style.height = "100px";
-    // console.log(response_json.image)
-    user_image_img.src = "http://127.0.0.1:8000" + response_json.image
+
+    user_image_img.src = "<http://127.0.0.1:8000>" + response_json.image // 프로필 이미지 소스 설정
     user_image_img.setAttribute("alt","user image")
-    user_image.appendChild(user_image_img)
+    user_image.appendChild(user_image_img) // 프로필 이미지 엘리먼트에 추가
 }
 
-    
-    
-    
+// 팔로우 처리 함수
 async function handleFollow() {
     if (localStorage.getItem("access"))
 
-        var user_id = userIdSearch()
+        var user_id = userIdSearch() // 유저 아이디 가져오기
 
         const response = await fetch(`http://127.0.0.1:8000/user/follow/${user_id}/`, {
             method : 'POST',
             headers: {
                 "Authorization" : "Bearer " + localStorage.getItem("access"),
             },
-            
+
         })
-        console.log(response)
-    location.reload()
+    location.reload() // 페이지 리로드
 }
--- static/js/user/profile.js
+++ static/js/user/profile.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    loadProfile()
+    loadProfile() // 프로필 정보를 불러오는 함수 호출
 }
 
 async function loadProfile() {
@@ -7,7 +7,6 @@ async function loadProfile() {
     const payload_parse = JSON.parse(payload)
     const request_user_id = payload_parse.user_id
 
-    console.log(request_user_id)
     const response = await fetch(`http://127.0.0.1:8000/user/profile/${request_user_id}/`, {
         method: 'GET',
         headers: {
@@ -15,16 +14,15 @@ async function loadProfile() {
         },
     })
     const response_json = await response.json()
-    console.log((response_json))
 
-    const username_json = document.getElementById("update_username")
-    const nickname = document.getElementById("update_nickname")
-    const email = document.getElementById("update_email")
-    const user_image = document.getElementById("image")
-    username_json.innerText = `${response_json.username}`
-    nickname.setAttribute("value", `${response_json.nickname}`)
-    email.setAttribute("value", `${response_json.email}`)
-    user_image.src = 'http://127.0.0.1:8000' + response_json.image
+    const username_json = document.getElementById("update_username") // HTML 요소의 id가 "update_username"인 요소를 가져옴
+    const nickname = document.getElementById("update_nickname")      // HTML 요소의 id가 "update_nickname"인 요소를 가져옴
+    const email = document.getElementById("update_email")            // HTML 요소의 id가 "update_email"인 요소를 가져옴
+    const user_image = document.getElementById("image")              // HTML 요소의 id가 "image"인 요소를 가져옴
+    username_json.innerText = `${response_json.username}`            // "update_username" 요소의 텍스트를 response_json의 username 값으로 설정
+    nickname.setAttribute("value", `${response_json.nickname}`)      // "update_nickname" 요소의 value 속성을 response_json의 nickname 값으로 설정
+    email.setAttribute("value", `${response_json.email}`)            // "update_email" 요소의 value 속성을 response_json의 email 값으로 설정
+    user_image.src = '<http://127.0.0.1:8000>' + response_json.image // "image" 요소의 src 속성을 '<http://127.0.0.1:8000>' + response_json의 image 값으로 설정
 
 }
 
@@ -35,10 +33,10 @@ async function handleUpdateProfile() {
 
     const formData = new FormData();
 
-    formData.append("email", document.getElementById("update_email").value);
-    formData.append("nickname", document.getElementById("update_nickname").value);
+    formData.append("email", document.getElementById("update_email").value);        // "update_email" 요소의 value 값을 FormData에 추가
+    formData.append("nickname", document.getElementById("update_nickname").value);  // "update_nickname" 요소의 value 값을 FormData에 추가
     if (document.getElementById("update_image").files[0]) {
-        formData.append("image", document.getElementById("update_image").files[0]);
+        formData.append("image", document.getElementById("update_image").files[0]); // "update_image" 요소의 파일을 FormData에 추가
     }
 
     const response = await fetch(`http://127.0.0.1:8000/user/profile/${request_user_id}/`, {
@@ -46,30 +44,27 @@ async function handleUpdateProfile() {
         headers: {
             "Authorization": "Bearer " + localStorage.getItem("access")
         },
-        body: formData,
+        body: formData, // FormData를 요청의 본문으로 설정
     });
 
-    console.log(response)
     if (response.status == 200) {
-        location.reload()
+        location.reload() // 페이지 새로고침
     }
     else if (response.status == 403) {
         const errorData = await response.json();
-        document.getElementById("profile_update_errors").innerText = errorData.message;
+        document.getElementById("profile_update_errors").innerText = errorData.message; // "profile_update_errors" 요소의 텍스트를 에러 메시지로 설정
     } else {
-        document.getElementById("profile_update_errors").innerText = "회원정보 수정 실패.";
+        document.getElementById("profile_update_errors").innerText = "회원정보 수정 실패."; // "profile_update_errors" 요소의 텍스트를 "회원정보 수정 실패."로 설정
     }
 }
 
 function hadleProfilePreview(input) {
-    console.log(input.files)
     if (input.files && input.files.length > 0) {
-        var previewContainer = document.getElementById('update_image');
+        var previewContainer = document.getElementById('update_image'); // HTML 요소의 id가 "update_image"인 요소를 가져옴
 
         var reader = new FileReader();
         reader.onload = function (e) {
-            document.getElementById("image").src = e.target.result;
-            // console.log(e.target.result)
+            document.getElementById("image").src = e.target.result; // "image" 요소의 src 속성을 FileReader의 결과로 설정
         };
         reader.readAsDataURL(input.files[0]);
     }
